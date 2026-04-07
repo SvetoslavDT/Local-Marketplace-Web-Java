9c6d422853d84054c8254f568bc1c5de0711e385
SvetoslavDT <svetcvetkov@gmail.com>
2026-04-07 23:06:36 +0300

    added the spring framework

---
+++ LocalMarketplace/src/main/java/bg/sofia/uni/fmi/localmarketplace/LocalMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.localmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalMarketplaceApplication.class, args);
+    }
+
+}
---
+++ LocalMarketplace/src/test/java/bg/sofia/uni/fmi/localmarketplace/LocalMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.localmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
