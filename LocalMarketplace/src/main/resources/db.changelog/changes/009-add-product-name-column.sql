40877425a4a86042bd189b020e9c50d4edc36204
SvetoslavDT <svetcvetkov@gmail.com>
2026-06-14 12:55:53 +0300

    Made some changes to the migrations

--- LocalMarketplace/src/main/resources/db.changelog/changes/009-add-product-name-column.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
-
---changeset fmi:009-add-product-name-column
-ALTER TABLE products ADD COLUMN name VARCHAR(100) NOT NULL DEFAULT 'Unnamed Product';
-
---rollback ALTER TABLE products DROP COLUMN name;
